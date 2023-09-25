6c5391242c6abce8f9914b9fcd42cfcfb37442dd
rachi <rachi@RASH>
2023-09-25 10:28:37 +0530

    updated Comments ClickHandleAnotherWaytest

--- src/test/java/CompName/RacJS/JavaScriptFrmaework/clickHandleAnotherWayTest.java
+++ src/test/java/CompName/RacJS/JavaScriptFrmaework/clickHandleAnotherWayTest.java
@@ -8,32 +8,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+//Updated comment
 public class clickHandleAnotherWayTest {
 
 	WebDriver driver;
+
 	@Test
 	public void atclickHandle() {
 		System.setProperty("webdriver.http.factory", "jdk-http-client");
 		String path = System.getProperty("user.dir");
-		
+
 		System.setProperty("webdriver.chrome.driver", path + "\\Driver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		driver.get("https://omayo.blogspot.com/");
 		driver.manage().window().maximize();
-		
-	// creating ref obj of JavAscrExt predefined interface and typecaste the webdriver
-		JavascriptExecutor js=(JavascriptExecutor)driver;
-	//Storing webelemnet 
-		WebElement clickBtn=driver.findElement(By.id("alert1"));
-		
+
+		// creating ref obj of JavAscrExt predefined interface and typecaste the
+		// webdriver
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		// Storing webelemnet
+		WebElement clickBtn = driver.findElement(By.id("alert1"));
+
 		js.executeScript("arguments[0].click()", clickBtn);
-		
-		Alert atr= driver.switchTo().alert();
+
+		Alert atr = driver.switchTo().alert();
 		atr.getText();
 		atr.accept();
 		driver.close();
-		
-		
+
 	}
 }
